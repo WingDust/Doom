c5c266fc663d6ee61e68400338e051d469ccef41
wingdust <995315823@qq.com>
2022-04-16 20:49:18 +0800

    packages.el

--- packages.el
+++ packages.el
@@ -129,7 +129,7 @@
 ;; (package! org-center-headings :recipe (:local-repo "~/.doom.d/local/org-center-headings.el"))
 ;; (add-load-path! "e:/spacemacs/emacs26-3/.doom.d/local/org-center-headings.el")
 (add-load-path! "e:/spacemacs/emacs26-3/.doom.d/local/")
-;; (require 'org-center-headings)
+(require 'org-center-headings)
 ;; (package! org-center-headings :recipe (:local-repo "e:/spacemacs/emacs26-3/.doom.d/local/org-center-headings.el"))
 
 ;;(package! ob-powershell :recipe (:local-repo "~/.doom.d/local/ob-powershell.el"))
